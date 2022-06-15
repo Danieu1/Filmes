3cf776d0fd816a273a44df4a1beba2dd834b08f6
Danieu1 <100950064+Danieu1@users.noreply.github.com>
2022-06-15 09:29:58 -0300

    Delete movie directory

--- movie/src/main/java/movies/demo/repository/FilmesRepository.java
+++
@@ -1,7 +0,0 @@
-package movies.demo.repository;
-import movies.demo.model.Filmes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmesRepository extends JpaRepository<Filmes, Long> {
-
-}
